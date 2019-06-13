86a36802c5b06de0d52fc6b3a98c9ab746ecc7dd
KwanghyunOn <40454531+KwanghyunOn@users.noreply.github.com>
2019-06-13 15:26:00 +0900

    Update Ternary Search.cpp

--- Ternary Search.cpp
+++ Ternary Search.cpp
@@ -1,132 +1,110 @@
 // https://codeforces.com/problemset/problem/106/E
-// source code by wRabbits_AlMag
 
-#include <iostream>
-#include <set>
-#include <vector>
-#include <map>
-#include <algorithm>
-#include <cstdio>
-#include <memory.h>
-
+#include <bits/stdc++.h>
+ 
 using namespace std;
 
-#define FOR(i, a, b) for(int i=(a), _b(b); i<_b; ++i)
-#define FORD(i, b, a) for(int i=(b)-1, _a(a); i>=_a; --i)
-#define SZ(a) (int)(a).size()
-#define FILL(a, b) memset(a, b, sizeof(a))
-#define SQR(a) ((a)*(a))
+typedef long long ll;
+typedef pair<int, int> Pi;
+typedef pair<ll, ll> Pll;
+ 
+#define rep(pos, len) for(int pos=0;pos<len;pos++)
+#define repp(pos, len) for(int pos=1;pos<=len;pos++)
+ 
+#define INF 987654321
+#define IINF 2987654321987654321
+#define MOD 1000000009
 
-typedef long long LL;
-typedef pair<int, int> PII;
-typedef vector<int> VI;
+const double phi = (sqrt(5) + 1.0) / 2.0;
+const int itnum = 80;
 
-template<typename T>
-void Out(T res)
-{
-	cout << res << endl;
-	exit(0);
+inline double sq(double x) {
+	return x*x;
 }
 
-double phi = 1.618033989;
 
+
+const int MAXN = 105;
+double X[MAXN], Y[MAXN], Z[MAXN];
 int n;
-int X[111], Y[111], Z[111];
-
-double Find(double x, double y, double z)
-{
-	double res = 0;
-	FOR(i, 0, n)
-		res = max(res, SQR(x-1.*X[i])+SQR(y-1.*Y[i])+SQR(z-1.*Z[i]));
-	return res;
-}
-
-double FindZ(double x, double y, double& zz)
-{
-	double L = -10000, R = +10000, res, C1, C2;
-	int need = 3;
-	FOR(i, 0, 75)
-	{
-		double D = (R-L)/phi;
-		if (need&1) C1 = Find(x, y, L+D);
-		if (need&2) C2 = Find(x, y, R-D);
-		if (C1 < C2)
-		{
-			L = R-D, res = C1, zz = L+D;
-			C2 = C1; need = 1;
-		}
-		else
-		{
-			R = L+D, res = C2, zz = R-D;
-			C1 = C2; need = 2;
-		}
-	}
-	return res;
-}
-
-double FindYZ(double x, double& yy, double& zz)
-{
-	double L = -10000, R = +10000, res, Z1, Z2, C1, C2;
-	int need = 3;
-	FOR(i, 0, 75)
-	{
-		double D = (R-L)/phi;
-		if (need&1) C1 = FindZ(x, L+D, Z1);
-		if (need&2) C2 = FindZ(x, R-D, Z2);
-		if (C1 < C2)
-		{
-			L = R-D, res = C1, yy = L+D, zz = Z1;
-			C2 = C1; need = 1;
-		}
-		else
-		{
-			R = L+D, res = C2, yy = R-D, zz = Z2;
-			C1 = C2; need = 2;
-		}
-	}
-	return res;
-}
-
-double FindXYZ(double& xx, double& yy, double& zz)
-{
-	double L = -10000, R = +10000, res, Y1, Z1, Y2, Z2, C1, C2;
-	int need = 3;
-	FOR(i, 0, 75)
-	{
-		double D = (R-L)/phi;
-		if (need&1) C1 = FindYZ(L+D, Y1, Z1);
-		if (need&2) C2 = FindYZ(R-D, Y2, Z2);
-		if (C1 < C2)
-		{
-			L = R-D, res = C1, xx = L+D, yy = Y1, zz = Z1;
-			C2 = C1; need = 1;
-		}
-		else
-		{
-			R = L+D, res = C2, xx = R-D, yy = Y2, zz = Z2;
-			C1 = C2; need = 2;
-		}
-	}
-	return res;
-}
-
-int main()
-{
-#ifdef ALMAG_LOCAL
-	freopen("input.txt", "r", stdin);
-#else
-	//freopen("shooting.in", "r", stdin);
-	//freopen("shooting.out", "w", stdout);
-#endif
-
+void readInput() {
 	scanf("%d", &n);
-	FOR(i, 0, n) scanf("%d %d %d", X+i, Y+i, Z+i);
+	repp(i, n) scanf("%lf%lf%lf", &X[i], &Y[i], &Z[i]);
+}
 
+
+
+double find(double x, double y, double z) {
+	double mx = 0;
+	repp(i, n) mx = max(mx, sq(x - X[i]) + sq(y - Y[i]) + sq(z - Z[i]));
+	return mx;
+}
+
+double findZ(double x, double y, double &zz) {
+	double L = -1e4, R = 1e4, mx = 0, C1, C2;
+	int bm = 3;
+	rep(t, itnum) {
+		double D = (R - L) / phi;
+		if(bm & 1) C1 = find(x, y, R-D);
+		if(bm & 2) C2 = find(x, y, L+D);
+		if(C1 < C2) {
+			R = L+D;
+			mx = C1, zz = R-D;
+			C2 = C1, bm = 1;
+		} else {
+			L = R-D;
+			mx = C2, zz = L+D;
+			C1 = C2, bm = 2;
+		}
+	}
+	return mx;
+}
+
+double findYZ(double x, double &yy, double &zz) {
+	double L = -1e4, R = 1e4, mx = 0, C1, C2, Z1, Z2;
+	int bm = 3;
+	rep(t, itnum) {
+		double D = (R - L) / phi;
+		if(bm & 1) C1 = findZ(x, R-D, Z1);
+		if(bm & 2) C2 = findZ(x, L+D, Z2);
+		if(C1 < C2) {
+			R = L+D;
+			mx = C1, zz = Z1, yy = R-D;
+			C2 = C1, bm = 1;
+		} else {
+			L = R-D;
+			mx = C2, zz = Z2, yy = L+D;
+			C1 = C2, bm = 2;
+		}
+	}
+	return mx;
+}
+
+double findXYZ(double &xx, double &yy, double &zz) {
+	double L = -1e4, R = 1e4, mx = 0, C1, C2, Z1, Z2, Y1, Y2;
+	int bm = 3;
+	rep(t, itnum) {
+		double D = (R - L) / phi;
+		if(bm & 1) C1 = findYZ(R-D, Y1, Z1);
+		if(bm & 2) C2 = findYZ(L+D, Y2, Z2);
+		if(C1 < C2) {
+			R = L+D;
+			mx = C1, zz = Z1, yy = Y1, xx = R-D;
+			C2 = C1, bm = 1;
+		} else {
+			L = R-D;
+			mx = C2, zz = Z2, yy = Y2, xx = L+D;
+			C1 = C2, bm = 2;
+		}
+	}
+	return mx;
+}
+
+
+
+int main() {
+	readInput();
 	double xx, yy, zz;
-	double res = FindXYZ(xx, yy, zz);
-
-	//printf("%.12lf\n", res);
-	printf("%.12lf %.12lf %.12lf\n", xx, yy, zz);
-
-	return 0;
+	double mx = findXYZ(xx, yy, zz);
+	printf("%.11f %.11f %.11f\n", xx, yy, zz);
 }
