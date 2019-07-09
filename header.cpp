6c78384212bafb419a3c549ca357d63b5cfb5054
KwanghyunOn <40454531+KwanghyunOn@users.noreply.github.com>
2019-07-09 14:07:53 +0900

    Create header.cpp

---
+++ header.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+ 
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+ 
+#define rep(pos, len) for(int pos=0;pos<len;pos++)
+#define repp(pos, len) for(int pos=1;pos<=len;pos++)
+ 
+#define INF 1987654321
+#define IINF 87654321987654321
+#define MOD 1000000007
+
+// fast cin
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+}
