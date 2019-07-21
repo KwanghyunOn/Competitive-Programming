4e8b99a006220ce03807c08b1dab2bf644cd6ed6
KwanghyunOn <40454531+KwanghyunOn@users.noreply.github.com>
2019-07-21 17:04:50 +0900

    Update Articulation Point.cpp

--- Graph/Articulation Point.cpp
+++ Graph/Articulation Point.cpp
@@ -1,5 +1,6 @@
 int num[MAXN], low[MAXN], cur;
-bool cut[MAXN];
+bool cut[MAXN];		// cut[v]: true if v is articulation point
+vector<pii> bridge;	// list of bridges
 void dfs(int v, int p) {
 	int child = 0;
 	low[v] = num[v] = ++cur;
@@ -10,6 +11,7 @@ void dfs(int v, int p) {
 			dfs(c, v);
 			low[v] = min(low[v], low[c]);
 			if(p != -1 && low[c] >= num[v]) cut[v] = true;
+			if(low[c] > num[v]) bridge.emplace_back(min(v, c), max(v, c));
 		}
 	}
 	if(p == -1 && child > 1) cut[v] = true;
