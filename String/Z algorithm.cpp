aeca48b8a263945903f7695239f1975eac99c5ab
KwanghyunOn <40454531+KwanghyunOn@users.noreply.github.com>
2019-07-29 11:42:19 +0900

    Update Z algorithm.cpp

--- String/Z algorithm.cpp
+++ String/Z algorithm.cpp
@@ -1,21 +1,18 @@
-const int MAXN = 1e5 + 50;
-int z[MAXN];
-
-void calcZ(string &s) {
-	int L = 0, R = 0;
+void calcZ(const string &s, int *z) {
 	int n = (int)s.size();
+	int L = 0, R = 0;
 	for(int i = 1; i < n; i++) {
 		if(i > R) {
 			L = R = i;
-			while(R < n && s[R-L] == s[R]) R++;
-			z[i] = R-L; R--;
+			while(R+1 < n && s[R-L+1] == s[R+1]) R++;
+			z[i] = R-L+1;
 		} else {
 			int k = i-L;
 			if(z[k] < R-i+1) z[i] = z[k];
 			else {
 				L = i;
-				while(R < n && s[R-L] == s[R]) R++;
-				z[i] = R-L; R--;
+				while(R+1 < n && s[R-L+1] == s[R+1]) R++;
+				z[i] = R-L+1;
 			}
 		}
 	}
