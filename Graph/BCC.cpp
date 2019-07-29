5c476680a4907234daf70dd0a15283afb455d105
KwanghyunOn <40454531+KwanghyunOn@users.noreply.github.com>
2019-07-29 17:48:21 +0900

    Update BCC.cpp

--- Graph/BCC.cpp
+++ Graph/BCC.cpp
@@ -1,34 +1,28 @@
-// https://gist.github.com/koosaga/6f6fd50dd7067901f1b1
 // Vertex-disjoint BCC
-void dfs(int x, int p){
-	dfn[x] = low[x] = ++piv;
-	par[x] = p;
-	for(int i=0; i<graph[x].size(); i++){
-		int w = graph[x][i];
-		if(w == p) continue;
-		if(!dfn[w]){
-			dfs(w, x);
-			low[x] = min(low[x], low[w]);
-		}
-		else{
-			low[x] = min(low[x], dfn[w]);
+int num[MAXN], low[MAXN], CUR_TIME = 0;
+void dfs(int v, int p) {
+	num[v] = low[v] = ++CUR_TIME;
+	for(auto c : adj[v]) {
+		if(c == p) continue;
+		if(num[c]) low[v] = min(low[v], num[c]);
+		else {
+			dfs(c, v);
+			low[v] = min(low[v], low[c]);
 		}
 	}
 }
 
-void color(int x, int c){
-	if(c > 0) bcc[x].push_back(c);
-	vis[x] = 1;
-	for(int i=0; i<graph[x].size(); i++){
-		int w = graph[x][i];
-		if(vis[w]) continue;
-		if(dfn[x] <= low[w]){
-			bcc[x].push_back(++cpiv);
-			color(w, cpiv);
-		}
-		else{
-			color(w, c);
-		}
+vector<int> bcc[MAXN];
+int CUR_COL = 0;
+bool vis[MAXN];
+void color(int v, int col) {
+	bcc[v].push_back(col);
+	for(auto c : adj[v]) if(!vis[c]) {
+		vis[c] = true;
+		if(num[v] <= low[c]) {
+			bcc[v].push_back(++CUR_COL);
+			color(c, CUR_COL);
+		} else color(c, col);
 	}
 }
 
