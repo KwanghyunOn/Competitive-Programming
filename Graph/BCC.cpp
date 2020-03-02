cead5f1990a4eec8d3f4438d6ff1274ec3f069de
KwanghyunOn <40454531+KwanghyunOn@users.noreply.github.com>
2020-03-03 00:20:38 +0900

    Update BCC.cpp

--- Graph/BCC.cpp
+++ Graph/BCC.cpp
@@ -16,9 +16,9 @@ vector<int> bcc[MAXN];
 int CUR_COL = 0;
 bool vis[MAXN];
 void color(int v, int col) {
+	vis[v] = true;
 	bcc[v].push_back(col);
 	for(auto c : adj[v]) if(!vis[c]) {
-		vis[c] = true;
 		if(num[v] <= low[c]) {
 			bcc[v].push_back(++CUR_COL);
 			color(c, CUR_COL);
@@ -44,9 +44,9 @@ void dfs(int v, int p) {
 int bcc[MAXN], CUR_COL = 0;
 bool vis[MAXN];
 void color(int v, int col) {
+	vis[v] = true;
 	bcc[v] = col;
 	for(auto c : adj[v]) if(!vis[c]) {
-		vis[c] = true;
 		if(low[c] > num[v]) color(c, ++CUR_COL);
 		else color(c, col);
 	}
